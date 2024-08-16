e21f48b71b7a35be56c8be0e552e8f790d053ec3
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-08-16 11:27:50 +0200

    Implement BrowserRouter

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,24 @@
-
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import CreateGroceryPage from "./components/pages/CreateGroceryPage.tsx";
+import GroceryPage from "./components/pages/GroceryPage.tsx";
+import DetailedGroceryPage from "./components/pages/DetailedGroceryPage.tsx";
+import LoginPage from "./components/pages/LoginPage.tsx";
+import EditGroceryPage from "./components/pages/EditGroceryPage.tsx";
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="grocery" element={<GroceryPage/>}/>
+                    <Route path="grocery/:groceryId" element={<DetailedGroceryPage/>}/>
+                    <Route path="login" element={<LoginPage/>}/>
+                    <Route path="post" element={<CreateGroceryPage/>}/>
+                    <Route path="update/:groceryId" element={<EditGroceryPage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
-
 export default App
